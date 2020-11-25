3f7bd50fa4801afccee7563aff3e7cc85c473797
ifer-itcast <yangkangkang@itcast.cn>
2020-11-25 11:42:58 +0800

    修改打包之后的文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const path = require('path');
+
 module.exports = {
-    mode: 'development' // 开发版本，production => 压缩
+    mode: 'development', // 开发版本，production => 压缩
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist') , // 指定的是一个目录
+        filename: 'bundle.js' // 指定的是生成的文件名
+    }
 };
